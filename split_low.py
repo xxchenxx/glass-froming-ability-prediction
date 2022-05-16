27e8f7fb6fd534d1437e9713d23dec3ca6d61d11
xxchenxx <hfercxx@hotmail.com>
2022-05-16 23:22:27 +0000

    update

---
+++ split_low.py
@@ -0,0 +1,19 @@
+import torch
+import pandas 
+import pickle
+import numpy as np
+
+X, Y = pickle.load(open("low_data.pkl", "rb"))
+
+import torch
+from sklearn.model_selection import KFold
+
+
+import pickle
+kfold = KFold(n_splits=10, shuffle=True)
+
+
+for split_id, (train_id, test_id) in enumerate(kfold.split(X, Y)):
+    current_split = {"train_Xs": X[train_id], "train_labels": Y[train_id], "test_Xs": X[test_id], "test_labels": Y[test_id]}
+    pickle.dump(current_split, open(f"low_data_split.pkl", "wb"))
+    break
